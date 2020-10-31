f4e237707d772450b073aea38f40bf57821f99b5
Zach G <Zach.the.gecko@gmail.com>
2020-10-31 10:04:53 -0600

    changed upgrade.js to include buttons to go to the next modules

--- upgrade.js
+++ upgrade.js
@@ -1,4 +1,22 @@
 window.onload = function () {
+  const getParentJavascriptVariable = (variable, variableName) => {
+    const script = document.createElement("script");
+
+    script.id = variable;
+    script.textContent = `document.getElementById("${variable}").innerHTML = ENV.${variable} ? JSON.stringify(ENV.${variable}) : null`;
+
+    document.body.appendChild(script);
+
+    const jsonX = document.getElementById(variable).innerHTML;
+
+    if (jsonX) {
+      const x = JSON.parse(jsonX);
+      chrome.storage.local.set({
+        [variableName]: typeof x === "object" ? Object.values(x) : x,
+      });
+    }
+  };
+
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DASHBOARD ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   if (window.location.pathname === "/") {
     // Removes To-Do List
@@ -7,24 +25,38 @@ window.onload = function () {
     )[0];
     if (toDo) toDo.innerHTML = "";
 
-    // Gets Courses
-    const script = document.createElement("script");
-
-    script.id = "Get_ENV_Variable";
-    script.textContent = `document.getElementById("Get_ENV_Variable").innerHTML = ENV.STUDENT_PLANNER_COURSES ? JSON.stringify(ENV.STUDENT_PLANNER_COURSES) : null`;
-
-    document.head.appendChild(script);
-
-    jsonCourses = document.getElementById("Get_ENV_Variable").innerHTML;
-
-    if (jsonCourses) {
-      const courses = JSON.parse(jsonCourses);
-      // Stores Courses
-      chrome.storage.local.set({ courses: courses });
-    }
+    getParentJavascriptVariable("STUDENT_PLANNER_COURSES", "courses");
   }
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MODULES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  if (window.location.pathname === "/courses/:course/modules") {
-    console.log(course);
+  if (window.location.pathname.includes("modules")) {
+    getParentJavascriptVariable("COURSE_ID", "currentCourseID");
+
+    const createButton = (ID, words) => {
+      const button = document.createElement("a");
+      button.setAttribute(
+        "href",
+        `https://dsd.instructure.com/courses/${ID}/modules`
+      );
+      button.setAttribute("role", "button");
+      button.setAttribute("class", "Button");
+      button.innerHTML = words;
+      document.querySelector("#main").appendChild(button);
+    };
+
+    chrome.storage.local.get(["courses", "currentCourseID"], (result) => {
+      result.courses.reduce((acc, course) => {
+        if (course.id === result.currentCourseID && acc) {
+          const previouseCourseID = acc;
+          createButton(previouseCourseID, "Previous");
+        }
+        if (acc === result.currentCourseID) {
+          const nextCourseID = course.id;
+          createButton(nextCourseID, "Next");
+        }
+
+        return course.id;
+      }, null);
+    });
   }
+  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ THE END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 };
